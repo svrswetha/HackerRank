617007cba7dc2ca12cef7dda86604da85900bc9a
Swetha Vijaya Raghavan <svrswetha@gmail.com>
2017-04-14 15:48:10 +0530

    Day 1_Data Types Commit

---
+++ SourceCode/Day 1_DataTypes.java
@@ -0,0 +1,13 @@
+        int result = scan.nextInt();
+        System.out.println(i+result);
+        
+        double di = scan.nextDouble();
+        String s2 = null;
+        System.out.println(d + di);
+         while(scan.hasNextLine()){
+             s2 =scan.nextLine();
+         }
+        String res = s.concat(s2);
+        System.out.println(res);
+    
+ 
