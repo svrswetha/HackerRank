359ea24d7ca73bf25a4e7c466b72b863081106a7
Swetha Vijaya Raghavan <svrswetha@gmail.com>
2017-04-14 20:39:25 +0530

    Day 2: Operators Commit

---
+++ SourceCode/Day 2_Operators.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.math.*;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+            double tipCal = (mealCost)*(tipPercent*0.01);
+        // Write your calculation code here.
+       double taxCal = (mealCost)*(taxPercent*0.01);
+             
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(mealCost+tipCal+taxCal);
+         System.out.println("The total meal cost is " +totalCost + " dollars.");
+        // Print your result
+    }
+}
