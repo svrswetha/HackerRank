00a61388f863caad400ebcc91b9c04b6e537a4e7
Swetha Vijaya Raghavan <svrswetha@gmail.com>
2017-04-17 00:13:16 +0530

    Day 9: Recursion Commit

---
+++ SourceCode/Day 9_Recursion.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+
+public static int factorial(int number) {
+    int fact =0;
+    if(number ==1){
+      return 1;
+    }
+    else{
+     fact =  number * factorial(number - 1);
+   
+    }
+     return fact;
+}
+    
+public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int N = sc.nextInt();
+        int f = factorial(N);
+        System.out.println(f);
+    }
+}
