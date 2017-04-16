76e11bfbcfbee4402e7bdbe687beea3e1b60541e
Swetha Vijaya Raghavan <svrswetha@gmail.com>
2017-04-17 00:19:15 +0530

    Day 10: Binary Numbers Commit

---
+++ SourceCode/Day 10_Binary_Numbers.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+  
+  private static void BinaryForm(int number) {
+     int binary[] = new int[40];
+     //int res[]=new int[40];
+     int count=0;
+     int index = 0;
+     while(number > 0){
+       binary[index++] = number%2;
+       number = number/2;
+     }
+      int cnt = 0,max=0;
+      for(int i=0;i<binary.length;i++){
+          cnt=0;
+      while(i<binary.length&&binary[i]==1){
+            cnt++;
+            i++;
+           }
+   if(cnt>max) 
+   max=cnt;
+   }
+   System.out.println(max);
+  }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        BinaryForm(n);
+     
+    }
+}
