060efd844b0da3c28058496ef623009d7bad2545
Swetha Vijaya Raghavan <svrswetha@gmail.com>
2017-04-17 00:10:07 +0530

    Day 8: Dictionaries and Maps Commit

---
+++ SourceCode/Day 8_Dictionaries_and_Maps.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+
+public class Solution{
+    public static void main(String []args){
+       
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        HashMap<String,Integer> PhoneBook = new HashMap<String,Integer>();
+        
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            PhoneBook.put(name,phone);
+            }
+        
+         while(in.hasNext()){
+            String s = in.next();
+            if(!PhoneBook.containsKey(s)) {
+              System.out.println("Not found");
+             
+            }
+            else {
+              System.out.println(s+ "="+ PhoneBook.get(s)); 
+            }
+            
+         } 
+        
+        in.close();
+    }
+      
+}
